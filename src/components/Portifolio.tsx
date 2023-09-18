5dadcdcf349ba04669d23722ed01fd5c3a57f0b0
Junior Oliveira Leandro <contato_jr@outlook.com>
2023-09-18 15:44:20 -0300

    section portifolio update

--- src/components/Portifolio.tsx
+++ src/components/Portifolio.tsx
@@ -1,8 +1,76 @@
+import Image from "next/image";
+
+import PortifolioHome from '@/assets/demo-home.png'
+import DigitalAgency from '@/assets/digital-agency.png'
+import CorporateAgency from '@/assets/corporate-agency.png'
+import DarkPersonal from '@/assets/dark-personal-portfolio-landing.png'
+import PersonalLandingPage from '@/assets/personal-portfolio.png'
+import ProfessionalService from '@/assets/professional-services.png'
+
 export function Portifolio() {
     return (
-        <section className="bg-secundary-blue w-full h-96 text-primary-gray">
-            <div className="flex flex-col justify-center items-center">
-                <h2 className="font-bold my-10 text-4xl">Sites e Projetos Desenvolvidos</h2>
+        <section className="bg-secundary-blue w-full h-full">
+            <div className="flex flex-col justify-center items-center my-10">
+                <span className="font-bold mt-4 bg-gradient-radial from-blue-700 via-purple-500 to-pink-600 text-transparent bg-clip-text">DEIXE O CÓDIGO COMIGO!</span>
+                <h2 className="font-bold mt-2 mb-5 text-4xl bg-gradient-radial from-blue-700 via-purple-500 to-pink-600 text-transparent bg-clip-text">SITES E PROJETOS DESENVOLVIDOS</h2>
+                <span className="font-semibold text-primary-gray text-xl">Escolha entre meus estilos ou personalize facilmente seu site seguindo suas ideias.</span>
+                <span className="font-semibold text-primary-gray text-xl">Mais demonstrações estarão disponíveis em breve.</span>
+            </div>
+
+
+            <div className="w-full flex items-center justify-around gap-6 px-8 cursor-pointer">
+                <div className="rounded-lg flex flex-col items-center bg-primary-blue hover:scale-[1.02] transition-transform">
+                    <Image
+                        src={PortifolioHome}
+                        alt="banner"
+                        className=" rounded-t-lg"
+                    />
+                    <span className="text-primary-gray p-2">TEXTO DE EXEMPLO</span>
+                </div>
+                <div className="rounded-lg flex flex-col items-center bg-primary-blue hover:scale-[1.02] transition-transform">
+                    <Image
+                        src={DigitalAgency}
+                        alt="banner"
+                        className=" rounded-t-lg"
+                    />
+                    <span className="text-primary-gray p-2">TEXTO DE EXEMPLO</span>
+                </div>
+                <div className="rounded-lg flex flex-col items-center bg-primary-blue hover:scale-[1.02] transition-transform">
+                    <Image
+                        src={DarkPersonal}
+                        alt="banner"
+                        className=" rounded-t-lg"
+                    />
+                    <span className="text-primary-gray p-2">TEXTO DE EXEMPLO</span>
+                </div>
+            </div>
+
+
+            <div className="w-full flex items-center justify-around gap-6 px-8 py-10 cursor-pointer">
+                <div className="rounded-lg flex flex-col items-center bg-primary-blue hover:scale-[1.02] transition-transform">
+                    <Image
+                        src={CorporateAgency}
+                        alt="banner"
+                        className=" rounded-t-lg"
+                    />
+                    <span className="text-primary-gray p-2">TEXTO DE EXEMPLO</span>
+                </div>
+                <div className="rounded-lg flex flex-col items-center bg-primary-blue hover:scale-[1.02] transition-transform">
+                    <Image
+                        src={PersonalLandingPage}
+                        alt="banner"
+                        className=" rounded-t-lg"
+                    />
+                    <span className="text-primary-gray p-2">TEXTO DE EXEMPLO</span>
+                </div>
+                <div className="rounded-lg flex flex-col items-center bg-primary-blue hover:scale-[1.02] transition-transform">
+                    <Image
+                        src={ProfessionalService}
+                        alt="banner"
+                        className=" rounded-t-lg"
+                    />
+                    <span className="text-primary-gray p-2">TEXTO DE EXEMPLO</span>
+                </div>
             </div>
         </section>
     )
